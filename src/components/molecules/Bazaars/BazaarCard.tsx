74cfed54a3cc6c25c41e00d4044b772276b7a46c
Md-Rizwan <rizwan@saglus.com>
2023-02-12 08:01:01 +0530

    Add Api

--- src/components/molecules/Bazaars/BazaarCard.tsx
+++ src/components/molecules/Bazaars/BazaarCard.tsx
@@ -20,56 +20,59 @@ const BazaarCard = () => {
   }, []);
 
   return (
-    <div
-      className={classes.root}
-      onClick={() => navigate("/electronicsbazaar")}
-    >
+    <>
+      {" "}
       {getAllBazar.map((mainData: any) => (
         <>
           <>{console.log(mainData.url)}</>
-          <div className={classes.container}>
-            <div className={classes.bazaarContainer}>
-              <div className={classes.bazaarCard}>
-                <img
-                  className="brandLogo"
-                  src={mainData.bazaar_image}
-                  alt={"Logo"}
-                />
-                <p className="headTitle">{mainData.bazaar_name}</p>
-                <p className="headSubtitle">{mainData.bazaar_description}</p>
-              </div>
+          <div
+            className={classes.root}
+            onClick={() => navigate("/electronicsbazaar")}
+          >
+            <div className={classes.container}>
+              <div className={classes.bazaarContainer}>
+                <div className={classes.bazaarCard}>
+                  <img
+                    className="brandLogo"
+                    src={mainData.bazaar_image}
+                    alt={"Logo"}
+                  />
+                  <p className="headTitle">{mainData.test}</p>
+                  <p className="headSubtitle">{mainData.bazaar_description}</p>
+                </div>
 
-              <div className={classes.wholeSellerContainer}>
-                <div className={classes.wholesellerCard}>
-                  <div className="container">
-                    <p className="headTitle">{mainData.wholesellers}</p>
-                    <p className="headSubtitle">Wholesellers</p>
-                  </div>
+                <div className={classes.wholeSellerContainer}>
+                  <div className={classes.wholesellerCard}>
+                    <div className="container">
+                      <p className="headTitle">{mainData.wholesellers}</p>
+                      <p className="headSubtitle">Wholesellers</p>
+                    </div>
 
-                  <div className="container">
-                    <p className="headTitle">{mainData.agents}</p>
-                    <p className="headSubtitle">Agents</p>
-                  </div>
+                    <div className="container">
+                      <p className="headTitle">{mainData.agents}</p>
+                      <p className="headSubtitle">Agents</p>
+                    </div>
 
-                  <div className="container">
-                    <p className="headTitle">{mainData.states}</p>
-                    <p className="headSubtitle">States</p>
+                    <div className="container">
+                      <p className="headTitle">{mainData.states}</p>
+                      <p className="headSubtitle">States</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className={classes.revinerContainer}>
-              <div className={classes.reveniewCard}>
-                <div className="container">
-                  <div>
-                    <p className="headTitle">Total Revenue Earned</p>
-                    <p className="headSubtitle">{mainData.earnings}</p>
-                  </div>
+              <div className={classes.revinerContainer}>
+                <div className={classes.reveniewCard}>
+                  <div className="container">
+                    <div>
+                      <p className="headTitle">Total Revenue Earned</p>
+                      <p className="headSubtitle">{mainData.earnings}</p>
+                    </div>
 
-                  <div>
-                    <p className="headTitle">No. of Bills</p>
-                    <p className="headSubtitle">{mainData.bills}</p>
+                    <div>
+                      <p className="headTitle">No. of Bills</p>
+                      <p className="headSubtitle">{mainData.bills}</p>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -77,7 +80,7 @@ const BazaarCard = () => {
           </div>
         </>
       ))}
-    </div>
+    </>
   );
 };
 
