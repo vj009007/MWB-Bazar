160fd836f45220c400eafeb72060275947e2e407
Md-Rizwan <rizwan@saglus.com>
2023-02-21 08:16:03 +0530

    Role page developer

---
+++ src/screens/RolePermission/RolesPermission.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { DashboardLayout } from "@/components/layouts";
+import { RoleSection } from "@/components/molecules/rulespermission/roleSection";
+import { useNavigate } from "react-router-dom";
+import { Box, Grid, Pagination, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import UserProfile from '@/static/images/User-profile.png';
+
+const RolesPermission = () => {
+  const navigate = useNavigate();
+
+  return (
+    <DashboardLayout>
+      <Box className="w-full">
+        <RoleSection />
+
+      </Box>
+    </DashboardLayout>
+  );
+};
+
+export default RolesPermission;
